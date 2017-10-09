ce08fe32e65d7908cf44ee26e76961f4c1e8792d
Nick Kleshinski <nick@skycloudsystems.com>
2017-10-09 14:56:47 -0700

    first commit

---
+++ CMakeFiles/basicServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basicServer.dir/basicServer.cxx.o"
+  "basicServer.pdb"
+  "basicServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basicServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
